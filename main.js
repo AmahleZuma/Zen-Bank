4d7c4ab91aa0198ef05cdeb98f0fa4fe782d741a
Amahle <amahlez798@gmail.com>
2025-09-07 16:48:51 +0200

    The popup form isn't working, but I will make it work

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const transactForm = document.getElementById("transact-form")
+
+
+function hideTransactForm(){
+    if (transactForm.style.display === "none"){
+        transactForm.style.display = "block";
+    } else {
+        transactForm.style.display = "none"
+    }
+}
